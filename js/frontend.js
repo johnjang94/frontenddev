26ddcf6ecfdbdcc1adfc6447dd8ae3b960bbcf78
John Jang <jscolbe9675@gmail.com>
2023-06-18 23:59:44 -0400

    thirteenth commit

--- js/frontend.js
+++ js/frontend.js
@@ -8,10 +8,50 @@ let clickMessage = document.querySelector(".click");
 const mainContent = document.getElementById("main-content");
 let mainNavigation = document.querySelector(".navigation-header");
 let profile = document.getElementById("profile-introduction");
-let backgroundAudio = document.getElementById("background-audio");
-let greetingAudio = new Audio("bgm/portfolio_greeting.mp3");
+// ============= BACKGROUND AUDIO CLIPS ===========
+const songs = [
+  {
+    title: `'Salt is My Sugar' by Marie Marie`,
+    path: "bgm/salt-is-my-sugar.mp3",
+  },
+  { title: `'Flower' by Blackpink Jisoo`, path: "bgm/Flower.mp3" },
+  {
+    title: `'Never Be Alone' by The Fat Rat`,
+    path: "bgm/never-be-alone.mp3",
+  },
+  { title: `'Yet To Come' by BTS`, path: "bgm/yet_to_come.mp3" },
+  { title: `'Fake Love EDM' by BTS`, path: "bgm/Fake_Love_EDM.mp3" },
+  { title: `'Stray Cats' by Stray Cats`, path: "bgm/stray-cats.mp3" },
+  { title: `'Idol Remix' by BTS`, path: "bgm/idol-remix.mp3" },
+  { title: `'Peaceful Piano' by Unknown`, path: "bgm/peaceful-piano.mp3" },
+  {
+    title: `'Let's Shut up and Dance' by Jason Derulo (ft. Lay & NCT 127)`,
+    path: "bgm/lets-dance.mp3",
+  },
+  { title: `'LAW' by Czaer`, path: "bgm/LAW.mp3" },
+  { title: `'Monoday' by The Fat Rat`, path: "bgm/monoday.mp3" },
+  { title: `'The Nights' by Avicii`, path: "bgm/the-nights.mp3" },
+  { title: `'Believer' by Imagine Dragon`, path: "bgm/Believer.mp3" },
+  { title: `'Dance Monkey' by Tones and I`, path: "bgm/dance-monkey.mp3" },
+  { title: `'Levitating' by Dua Lipa`, path: "bgm/Levitating.mp3" },
+];
+const randomIndex = Math.floor(Math.random() * songs.length);
+let audioElement = document.getElementById("audio");
+audioElement.src = songs[randomIndex].path;
 
-greetingAudio.play();
+let songTitle = document.getElementById("song-title");
+songTitle.textContent = `${songs[randomIndex].title}`;
+
+let currentIndex = Math.floor(Math.random() * songs.length);
+
+audioElement.src = songs[currentIndex].path;
+songTitle.textContent = songs[currentIndex].title;
+
+audioElement.addEventListener("ended", () => {
+  currentIndex = Math.floor(Math.random() * songs.length);
+  audioElement.src = songs[currentIndex].path;
+  songTitle.textContent = songs[currentIndex].title;
+});
 
 blackCover.addEventListener("click", () => {
   blackCover.style.transform = "translateY(-700px)";
@@ -65,13 +105,12 @@ const display = () => {
 
 addEventListener("DOMContentLoaded", () => {
   display();
-  backgroundAudio.pause();
   mainContent.style.display = "none";
+  audioElement.pause();
   blackCover.addEventListener("click", () => {
     mainContent.style.display = "block";
-    greetingAudio.pause();
-    backgroundAudio.play();
     window.scroll({ top: 0, behavior: "smooth" });
+    audioElement.play();
   });
 });
 
